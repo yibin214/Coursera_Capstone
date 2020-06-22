db77def2183c8444ad3058acdd70763f75483f0f
yibin214 <liang10301022@gmail.com>
2020-06-22 16:56:41 +0800

    add a python script that prints a message greeting Capstone

---
+++ Data_Science_Capstone_Project.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This markdown is for the data science capstone project of IBM Data Science Professional Certificate on Coursera.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
+# 
